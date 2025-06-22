a5649bda7ee7004b9365eda3e67ce0173b5abe1f
Aman Rayal <amanrayal288@gmail.com>
2025-06-22 21:46:10 +0530

    path error

--- src/features/user/services/userWallpaperService.js
+++ src/features/user/services/userWallpaperService.js
@@ -2,7 +2,7 @@ import API from "../../../services/api.js";
 
 export const getUserWallpapers = async (page = 1, limit = 20) => {
   try {
-    const res = await API.get(`/wallpapers/getUserWallpaper?page=${page}&limit=${limit}`);
+    const res = await API.get(`/api/v1/wallpapers/getUserWallpaper?page=${page}&limit=${limit}`);
     return res.data?.data?.wallpaper || [];
   } catch (error) {
     console.error("Error fetching user wallpapers:", error?.response?.data || error.message);
