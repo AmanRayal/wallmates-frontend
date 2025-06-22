e468628b62f94345bc3370aca64d1f27fc5b12b6
Aman Rayal <amanrayal288@gmail.com>
2025-06-22 21:08:06 +0530

    path error

--- src/features/user/pages/UploadWallpapers.jsx
+++ src/features/user/pages/UploadWallpapers.jsx
@@ -43,7 +43,7 @@ const UploadWallpaper = () => {
     try {
       setLoading(true);
 const res = await axios.post(
-  `${import.meta.env.VITE_API_URL}/wallpapers/uploadWallpapers`,
+  `${import.meta.env.VITE_API_URL}/api/v1/wallpapers/uploadWallpapers`,
   data,
         {
           withCredentials: true,
